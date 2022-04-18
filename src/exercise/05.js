72c6193f89e844bfe9d3d2439e53983a37e02d73
Harsha Amarasiri <harshasuranjith@gmail.com>
2022-04-18 10:51:19 +0530

    EX 5-2 Creating a custom component with styling: encapsulating styles with a custom size property

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,22 +14,48 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div className='box box--small' style={{backgroundColor: 'lightblue'}}>small lightblue box</div>
-const mediumBox = <div className='box box--medium' style={{backgroundColor: 'pink'}}>medium pink box</div>
-const largeBox = <div className='box box--large' style={{backgroundColor: 'orange'}}>large orange box</div>
+const smallBox = (
+  <div className="box box--small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </div>
+)
+const mediumBox = (
+  <div className="box box--medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </div>
+)
+const largeBox = (
+  <div className="box box--large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Box className="box--small" style={{backgroundColor: 'lightblue' }}>small lightblue box</Box>
-      <Box className="box--medium" style={{backgroundColor: 'pink' }}>medium pink box</Box>
-      <Box className="box--large" style={{backgroundColor: 'orange'}}>large orange box</Box>
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
 
-function Box({className, style, ...otherProps}){
-    return <div className={`box ${className}`}  style={{fontStyle: 'italic', ...style}} {...otherProps}></div>
+function Box({size, style, className = '', ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : ''
+
+  return (
+    <div
+      className={`box ${className} ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
 }
 
 export default App
